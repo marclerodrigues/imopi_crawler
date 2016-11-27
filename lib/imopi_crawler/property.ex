3edb10902dea8017c5ae9fcbc4159a0ea90a9b79
Marcle Rodrigues <marcle.rodrigues@codeminer42.com>
2016-11-27 16:59:27 -0300

    Refactor property for better readability

--- lib/imopi_crawler/property.ex
+++ lib/imopi_crawler/property.ex
@@ -1,3 +1,13 @@
 defmodule ImopiCrawler.Property do
-  defstruct [:image, :price, :zone, :rooms, :parking_space, :bathrooms, :area, :cost]
+  defstruct [
+    :image,
+    :price,
+    :zone,
+    :rooms,
+    :parking_space,
+    :bathrooms,
+    :area,
+    :cost,
+    :details_url
+  ]
 end
